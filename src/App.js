5563e8a896f44a371774adaa960c81f9c2c29546
rogelioltz <rogelio.romero@integrarh.mx>
2020-11-23 20:56:31 -0700

    feat: terminado front end

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [listNumbers, setListNumbers] = useState('');
+  const sendListNumber = async (e) => {
+    try {
+      e.preventDefault();
+      const listNumberToArray = listNumbers.split(',').map((element) => {
+        return parseInt(element);
+      });
+      console.log(listNumberToArray);
+      const {
+        data: { result },
+      } = await axios.post('http://localhost:3001/api/order-numbers', {
+        listNumbers: listNumberToArray,
+      });
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const addingListNumbers = ({ target }) => {
+    setListNumbers(target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <form onSubmit={sendListNumber}>
+        <label htmlFor='list'>Agrega tu lista de numeros</label>
+        <input
+          placeholder='Introdusca su lista numeros'
+          name='list'
+          value={listNumbers}
+          onChange={addingListNumbers}
+        />
+        <button type='submit'>Ordenar Lista </button>
+      </form>
     </div>
   );
 }
